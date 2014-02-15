ab275111f379641a83ec78ab0b8b9456ff018966
Eko Wahyudi <eko.wahyudi15@outlook.com>
2014-02-15 13:47:29 +0700

    Perbaikan hal Tentang
    tambahan: Email Feedback

--- Muslimeter3/Second Pages/Tentang.xaml.vb
+++ Muslimeter3/Second Pages/Tentang.xaml.vb
@@ -1,7 +1,24 @@
-﻿Partial Public Class Page1
-    Inherits PhoneApplicationPage
+﻿Imports Microsoft.Phone.Tasks
 
+
+Partial Public Class Page1
+    Inherits PhoneApplicationPage
     Public Sub New()
         InitializeComponent()
+
+
+
+    End Sub
+
+    Private Sub hyperlink_Click(sender As Object, e As RoutedEventArgs)
+        Dim emailComposeTask As EmailComposeTask = New EmailComposeTask()
+
+        emailComposeTask.Subject = "message subject"
+        emailComposeTask.Body = "message body"
+        emailComposeTask.To = "wahyudi_eko@if.undip.ac.id"
+        'emailComposeTask.Cc = "cc@example.com"
+        'emailComposeTask.Bcc = "bcc@example.com"
+
+        emailComposeTask.Show()
     End Sub
 End Class
